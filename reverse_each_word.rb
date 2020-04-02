8d39eaf3edd467b237b31235725ca2778eda2a2c
bruce willis <fashun_vyctum@yahoo.com>
2020-04-02 00:30:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,10 +7,10 @@ end
 new.join(" ")
 end
 
-def reverse_each_word_with_collect(sentence2)
+def reverse_each_word(sentence2)
   array = sentence2.split(" ")
   test_array = []
-  array.collect do|string|
+  array.collect do|sentence2|
     test_array << sentence2.reverse
   end
   test_array.join(" ")
